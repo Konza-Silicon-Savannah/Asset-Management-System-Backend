2d969ee3d3a2bd8c86f30557df8178f6f344e24a
malenya <ianmalenya95@gmail.com>
2025-06-02 14:05:52 +0300

    asset module changes

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import AssetViewSet
+#yes
 
 router = routers.DefaultRouter()
 router.register("assets", AssetViewSet)
